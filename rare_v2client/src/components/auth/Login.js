eebddd420bcf75abbc3b0204b825778b06d08a37
Melody Barker <melodybarker2013@gmail.com>
2022-01-31 18:45:21 -0600

    login and auth

--- rare_v2client/src/components/auth/Login.js
+++ rare_v2client/src/components/auth/Login.js
@@ -12,7 +12,7 @@ export const Login = () => {
     const handleLogin = (e) => {
         e.preventDefault()
 
-        return fetch("http://127.0.0.1:8088/login", {
+        return fetch("http://127.0.0.1:8000/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
--- rare_v2client/src/components/auth/Register.js
+++ rare_v2client/src/components/auth/Register.js
@@ -22,7 +22,7 @@ export const Register = (props) => {
                 "password": password.current.value
             }
 
-            return fetch("http://127.0.0.1:8088/register", {
+            return fetch("http://127.0.0.1:8000/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
